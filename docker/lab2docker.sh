fcf07f39af93b3913c4a9a207262fdf2eb2a9b94
Luke Biddell <luke.a.biddell@gmail.com>
2018-02-16 17:11:03 +0000

    Fixed docker setup sh file

--- docker/lab2docker.sh
+++ docker/lab2docker.sh
@@ -1,6 +1,6 @@
-cd ..
+# Run instructions
+# sh docker/lab2docker.sh
 mkdir docker/niso2-lxb550/src
 mkdir docker/niso2-lxb550/bin
 cp -r src/lab2 src/helper docker/niso2-lxb550/src/
 cp -r bin/lab2 bin/helper docker/niso2-lxb550/bin/
-
