13e6aaa1c2cd0960351bb81c649ed2e1fb821477
Luke Biddell <luke.a.biddell@gmail.com>
2018-03-16 22:59:32 +0000

    Updated docker

--- docker/dockersetup.sh
+++ docker/dockersetup.sh
@@ -5,15 +5,23 @@ then
 	echo "There should be 1 argument for lab number"
 elif [ "$1" = "2" ]
 then
+	rm -r docker/niso2-lxb550/src
 	mkdir -p docker/niso2-lxb550/src
 	cp -r src/lab2 src/helper docker/niso2-lxb550/src/
 	echo "Lab 2 files are ready for Docker"
 	
 elif [ "$1" = "3" ]
 then
+	rm -r docker/niso3-lxb550/src
 	mkdir -p docker/niso3-lxb550/src
-	cp -r src/lab3 src/helper docker/niso3-lxb550/src/
+	cp -r src/lab3 docker/niso3-lxb550/src/
 	echo "Lab 3 files are ready for Docker"
+elif [ "$1" = "4" ]
+then
+	rm -r docker/niso4-lxb550/src
+	mkdir -p docker/niso4-lxb550/src
+	cp -r src/lab4 docker/niso4-lxb550/src/
+	echo "Lab 4 files are ready for Docker"
 else
 	echo "Argument should be a lab number"
 fi
--- logs/lab3/lab3_commands.R
+++ logs/lab3/lab3_commands.R
@@ -11,19 +11,19 @@ lambda <- read.delim("~/Documents/eclipse-workspace/niso-labs/logs/lab3/15206157
 mutationrate <- read.delim("~/Documents/eclipse-workspace/niso-labs/logs/lab3/1520615718148mutationrate.tsv")
 
 boxplot(b~type, data=algorithmtype, main="Average Number of Individuals in Bar vs Algorithm Type", xlab="Algorithm Type", ylab="Average Number of Individuals in Bar")
-dev.print(device = png, filename = 'algorithmtype2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'algorithmtype3.png', width = 600, height = 600)
 
 boxplot(b~h, data=h, main="Average Number of Individuals in Bar vs Number of States per Strategy", xlab="Number of States per Strategy", ylab="Average Number of Individuals in Bar")
-dev.print(device = png, filename = 'h2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'h3.png', width = 600, height = 600)
 
 boxplot(b~k, data=k, main="Average Number of Individuals in Bar vs Tournament Size", xlab="Tournament Size", ylab="Average Number of Individuals in Bar")
-dev.print(device = png, filename = 'k2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'k3.png', width = 600, height = 600)
 
 boxplot(b~lambda, data=lambda, main="Average Number of Individuals in Bar vs Population Size", xlab="Population Size", ylab="Average Number of Individuals in Bar")
-dev.print(device = png, filename = 'lambda2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'lambda3.png', width = 600, height = 600)
 
 boxplot((b/lambda)~lambda, data=lambda, main="Average Percentage of Individuals in Bar vs Population Size", xlab="Population Size", ylab="Average Percentage of Individuals in Bar")
-dev.print(device = png, filename = 'lambdapercent2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'lambdapercent3.png', width = 600, height = 600)
 
 boxplot(b~chi, data=mutationrate, main="Average Number of Individuals in Bar vs Mutation Rate", xlab="Mutation Rate", ylab="Average Number of Individuals in Bar")
-dev.print(device = png, filename = 'mutationrate2.png', width = 600, height = 600)
+dev.print(device = png, filename = 'mutationrate3.png', width = 600, height = 600)
