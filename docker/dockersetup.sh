ed5b4ffc5184bcedabfb9e96c14e8997e4b230d2
Luke Biddell <luke.a.biddell@gmail.com>
2018-03-16 22:36:12 +0000

    Fixed docker setup

--- docker/dockersetup.sh
+++ docker/dockersetup.sh
@@ -8,10 +8,11 @@ then
 	mkdir -p docker/niso2-lxb550/src
 	cp -r src/lab2 src/helper docker/niso2-lxb550/src/
 	echo "Lab 2 files are ready for Docker"
+	
 elif [ "$1" = "3" ]
 then
 	mkdir -p docker/niso3-lxb550/src
-	cp -r src/lab3 src/helper src/elFarolBar docker/niso3-lxb550/src/
+	cp -r src/lab3 src/helper docker/niso3-lxb550/src/
 	echo "Lab 3 files are ready for Docker"
 else
 	echo "Argument should be a lab number"
